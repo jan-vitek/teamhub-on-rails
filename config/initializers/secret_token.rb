3640e477a3bc8b26d759d1fe780f857f40baf566
Jan Vítek <jan.vitek@virtualmaster.com>
2013-02-03 15:13:05 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeamhubOnRails::Application.config.secret_token = 'c156e764ff9d39a09b6efcbd38657fc3dd0b040918ff6de12bd6a45c94e50a82449fa290ec5286f0268c47ab4d8be93e30f89f97e36b4d26d7a2659cc943be46'
